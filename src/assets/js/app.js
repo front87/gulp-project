6aff02a1d3b0999e57767d3e3ab553c7e71ffe7c
front87 <sergey.k@whmcs.com.ua>
2017-03-13 21:51:26 +0200

    added old project

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,43 @@
-$(document).foundation();
+//$(document).foundation()
+
+$(document).ready(function(){
+    $('.portfolio-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.header-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        fade: true,
+        cssEase: 'linear',
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1170,
+                settings: "unslick"
+            }
+        ]
+    });
+});
