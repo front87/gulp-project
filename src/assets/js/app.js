4dd0e2544a84c3b793279e52ebd1be83dece2dc1
front87 <sergey.k@whmcs.com.ua>
2017-03-17 23:03:02 +0200

    completed contact page

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -40,4 +40,28 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $('.team-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
